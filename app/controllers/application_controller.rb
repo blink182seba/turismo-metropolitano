89bc91c79db844c5464f9d3a83b6f80558662425
Sebastian <blink182seba@gmail.com>
2018-10-26 20:28:00 -0300

    terminado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
        format.html { redirect_to root_url, alert: exception.message }
